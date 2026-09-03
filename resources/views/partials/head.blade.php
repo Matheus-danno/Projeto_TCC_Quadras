2c2198fdfe9a7b5393e637effad6d47fc612592e
GabyAlves25 <gabyalveswhats@gmail.com>
2026-09-03 19:23:14 -0300

    Faz o painel iniciar no tema claro por padrao

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -13,4 +13,9 @@
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
+<script>
+    if (!window.localStorage.getItem('flux.appearance')) {
+        window.localStorage.setItem('flux.appearance', 'light')
+    }
+</script>
 @fluxAppearance
